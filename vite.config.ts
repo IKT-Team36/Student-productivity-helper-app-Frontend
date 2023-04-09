5a418a3c57b714a62c817bff27ad1e0f58500cf2
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-04-09 23:45:55 +0200

    Project env setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const projectRootDir = path.resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: 'src', replacement: path.resolve(projectRootDir, 'src') }],
+  },
 })
