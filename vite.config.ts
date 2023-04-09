134e55974adf9161af92fcad22424268dacda053
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-04-09 23:59:00 +0200

    Project env setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ const projectRootDir = path.resolve(__dirname)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: [{ find: 'src', replacement: path.resolve(projectRootDir, 'src') }],
   },
