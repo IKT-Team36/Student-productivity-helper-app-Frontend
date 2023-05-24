7944669ce32419ec17bf9bb0950aa5a186ff240c
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-05-24 22:12:13 +0200

    Update note

--- src/ui/pages/Notes.tsx
+++ src/ui/pages/Notes.tsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import {ScreenLayout} from "@src/ui/layout/main-layout/ScreenLayout";
-import {Box, Button, TextField, Modal, Typography, styled, useTheme} from "@mui/material";
+import {
+    Box, Button, TextField, Typography, styled, Dialog, DialogTitle, DialogContent, DialogActions
+} from "@mui/material";
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {DemoContainer} from '@mui/x-date-pickers/internals/demo';
 import {DateRangePicker} from "@mui/x-date-pickers-pro/DateRangePicker";
 
-const style = {
-    position: 'absolute' as 'absolute',
-    top: '40%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 800,
-    bgcolor: 'background.paper',
-    border: '1px solid #000',
-    borderRadius: '5px',
-    boxShadow: '1px 1px 50px #8e8e8e',
-    p: 4,
-};
-const TextFieldStyled = styled(TextField)(({theme}) => ({
+const TextFieldStyled = styled(TextField)(() => ({
     '& #outlined-multiline-static-label, #standard-basic-label': {
         color: '#dfdfdf'
     }
 }));
-const DatepPickerStyled = styled(DateRangePicker)(({theme}) => ({
+const DatepPickerStyled = styled(DateRangePicker)(() => ({
     '& .MuiFormControl-root label': {
         color: '#dfdfdf'
     }
@@ -32,12 +22,10 @@ const DatepPickerStyled = styled(DateRangePicker)(({theme}) => ({
 
 
 export const Notes = () => {
-    const theme = useTheme()
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    return (
-        <ScreenLayout title={'Notes'}>
+    return (<ScreenLayout title={'Notes'}>
             <Box>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
                     Create New Note
@@ -47,43 +35,41 @@ export const Notes = () => {
                     <Button onClick={handleOpen} sx={{position: 'relative', py: '165px', px: '155px'}}>
                         <ControlPointIcon style={{opacity: '20%'}} sx={{fontSize: 84}}/>
                     </Button>
-                    <Modal
-                        open={open}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                        sx={{scale: '100%'}}
-                    >
-                        <Box component="form" sx={style} noValidate autoComplete="off">
-                            <Typography id="modal-modal-title" variant="h6" component="h2">
-                                Create New Note
-                            </Typography>
-                            <TextFieldStyled sx={{mt: 3}} id="standard-basic" label="Title" variant="outlined"/>
-                            <Typography id="modal-modal-description" sx={{mt: 4}}>
-                                <TextFieldStyled
-                                    id="outlined-multiline-static"
-                                    label="Description"
-                                    multiline
-                                    rows={14}
-                                    variant="outlined"
-                                    sx={{width: '600px'}}/>
-                            </Typography>
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DemoContainer components={['DateRangePicker', 'DateRangePicker']}>
-                                    <DatepPickerStyled sx={{mt: 3}} slotProps={{
-                                        fieldSeparator: {
-                                            sx: {
-                                                opacity: 0.5,
-                                                color: 'gray'
+                    <Dialog onClose={handleClose} open={open}>
+                        <DialogTitle>Create new note</DialogTitle>
+                        <DialogContent>
+                            <Box component="form" noValidate autoComplete="off">
+                                <TextFieldStyled sx={{width: '50%'}} id="standard-basic" label="Title"
+                                                 variant="outlined"/>
+                                <Typography id="modal-modal-description" sx={{mt: 4}}>
+                                    <TextFieldStyled
+                                        id="outlined-multiline-static"
+                                        label="Description"
+                                        multiline
+                                        rows={14}
+                                        variant="outlined"
+                                        sx={{width: '100%'}}/>
+                                </Typography>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DemoContainer components={['DateRangePicker', 'DateRangePicker']}>
+                                        <DatepPickerStyled sx={{mt: 3}} slotProps={{
+                                            fieldSeparator: {
+                                                sx: {
+                                                    opacity: 0.5, color: 'gray'
+                                                }
                                             }
-                                        }
-                                    }}/>
-                                </DemoContainer>
-                            </LocalizationProvider>
-                            < Button variant="outlined" sx={{float:'right',mt:3}} onClick={handleClose}>Close</Button>
-                            < Button variant="outlined" sx={{float:'right',mt:3, mr:1}} onClick={handleClose}>Save</Button>
-                        </Box>
-
-                    </Modal>
+                                        }}/>
+                                    </DemoContainer>
+                                </LocalizationProvider>
+                                <DialogActions sx={{mt: 3, p: 0}}>
+                                    <Button variant="outlined"
+                                            onClick={handleClose}>Close</Button>
+                                    <Button variant="contained"
+                                            onClick={handleClose}>Save</Button>
+                                </DialogActions>
+                            </Box>
+                        </DialogContent>
+                    </Dialog>
                 </Box>
             </Box>
         </ScreenLayout>
