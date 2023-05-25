71d55fdcb96e89e175138509323b491f28d5d71b
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-05-25 18:52:20 +0200

    initial components

---
+++ src/ui/layout-page/login/Login.tsx
@@ -0,0 +1,10 @@
+import React, {ReactElement} from "react";
+import {Box} from "@mui/material";
+
+export const Login = (): ReactElement => {
+    return (
+        <Box>
+
+        </Box>
+    )
+}
---
+++ src/ui/layout-page/register/Register.tsx
@@ -0,0 +1,10 @@
+import React, {ReactElement} from "react";
+import {Box} from "@mui/material";
+
+export const Register = (): ReactElement => {
+    return (
+        <Box>
+
+        </Box>
+    )
+}
