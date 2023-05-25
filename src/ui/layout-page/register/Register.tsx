f8e3fcb33dfe8dd64014c9dd333e3ee563d4ab19
Зендељи Шпреса <shpresa.zendelji@students.finki.ukim.mk>
2023-05-26 00:34:00 +0200

    Log in done

--- src/ui/layout-page/register/Register.tsx
+++ src/ui/layout-page/register/Register.tsx
@@ -162,12 +162,12 @@ export const Register = (): ReactElement => {
                   variant="contained"
                   sx={{ mt: 3, mb: 2 }}
                 >
-                  Sign Up
+                  Register
                 </Button>
                 <Grid container justifyContent="flex-end">
                   <Grid item>
                     <Link href="/login" variant="body2">
-                      Already have an account? Sign in
+                      Already have an account? Log in
                     </Link>
                   </Grid>
                 </Grid>
